9cb9288cbcba76e938803480dbbfcb4ecbe5c8d4
tjnickerson <tjnickerson@vt.edu>
2025-03-29 03:04:10 -0400

    Added circuits

--- editorObjects/EditorNode.js
+++ editorObjects/EditorNode.js
@@ -1,6 +1,9 @@
 class EditorNode {
-    constructor(x, y,width, height, symbol, inputs, outputs, ) {
-        this.inputs = inputs;
+    constructor(x, y, width, height, symbol, inputs, outputs,) {
+        this.inputs = []
+        for (let i = 0; i < inputs.length; i++) {
+            this.inputs.push({ label: inputs[i], conn: false })
+        }
         this.outputs = outputs;
         this.x = x;
         this.y = y;
@@ -15,27 +18,37 @@ class EditorNode {
     DrawLabels(x, y) {
 
     }
-    GetInputPosition(index){
-        return {x:this.x + (2*(1+ index) /(1 + this.inputs.length)-1) * this.width, y: this.y - this.height}
+    GetInputPosition(index) {
+        return { x: this.x + (2 * (1 + index) / (1 + this.inputs.length) - 1) * this.width, y: this.y - this.height }
     }
-    GetOutputPosition(index){
-        return {x:this.x + (2*(1+ index) /(1 + this.outputs.length)-1) * this.width, y: this.y + this.height}
+    GetOutputPosition(index) {
+        return { x: this.x + (2 * (1 + index) / (1 + this.outputs.length) - 1) * this.width, y: this.y + this.height }
     }
     Draw() {
-        push ()
+        image(this.image, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height)
+    }
+    DrawCircuit() {
+        push()
         //draw nodes
         stroke(0)
         strokeWeight(5)
-        for(let i = 0; i<this.inputs.length; i++){
-            line (this.x,this.y, this.GetInputPosition(i).x,this.GetInputPosition(i).y)
+        for (let i = 0; i < this.inputs.length; i++) {
+            line(this.x, this.y, this.GetInputPosition(i).x, this.GetInputPosition(i).y)
+            if (this.inputs[i].conn) {
+                let pos = this.inputs[i].conn.node.GetOutputPosition(this.inputs[i].conn.idx)
+                line(this.GetInputPosition(i).x, this.GetInputPosition(i).y,pos.x,pos.y)
+            }
         }
-        for(let i = 0; i<this.outputs.length; i++){
-            line (this.x,this.y, this.x + (2*(1+ i) /(1 + this.outputs.length)-1) * this.width, this.y + this.height)
+        for (let i = 0; i < this.outputs.length; i++) {
+            line(this.x, this.y, this.GetOutputPosition(i).x, this.GetOutputPosition(i).y)
         }
-        image(this.image, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height)
-        pop ()
+        pop()
     }
-    static getSymbol(){
+    ConnectInputToOutput(idx, other, otherIdx) {
+        console.log(0)
+        this.inputs[idx].conn = { node: other, idx: otherIdx }
+    }
+    static getSymbol() {
         return Assets.ham;
     }
     static DrawSymbol(x, y) {
--- guiObjects/EditorScreen.js
+++ guiObjects/EditorScreen.js
@@ -26,6 +26,20 @@ class EditorScreen extends GUI {
                     //delete node
                     this.tableObjects.splice(this.tableObjects.indexOf(this.itemSelected), 1);
                 }
+            } else if (this.itemTypeSelected == "inputNode") {
+
+                for (let i = 0; i < this.tableObjects.length; i++) {
+                    for (let j = 0; j < this.tableObjects[i].outputs.length; j++) {
+                        let pos = this.tableObjects[i].GetOutputPosition(j);
+                        if (dist(pos.x, pos.y, x - this.tableXOffset, y - this.tableYOffset) < 15
+                            //&& y < this.itemSelected.obj.GetInputPosition(this.itemSelected.idx).y + this.tableXOffset
+                        ) {
+                            this.itemSelected.obj.ConnectInputToOutput(this.itemSelected.idx, this.tableObjects[i], j);
+
+                        }
+
+                    }
+                }
             }
             this.itemSelected = false;
         }
@@ -33,6 +47,9 @@ class EditorScreen extends GUI {
         background(220)
         push()
         translate(this.tableXOffset, this.tableYOffset)
+        for (let i = 0; i < this.tableObjects.length; i++) {
+            this.tableObjects[i].DrawCircuit();
+        }
         for (let i = 0; i < this.tableObjects.length; i++) {
             this.tableObjects[i].Draw();
             if (!this.itemSelected) {
@@ -43,7 +60,7 @@ class EditorScreen extends GUI {
                     }
                 }
             }
-            if (!this.itemSelected) {
+            if ((this.itemTypeSelected == "inputNode" && this.itemSelected) || !this.itemSelected) {
                 for (let j = 0; j < this.tableObjects[i].outputs.length; j++) {
                     let pos = this.tableObjects[i].GetOutputPosition(j);
                     if (dist(pos.x, pos.y, x - this.tableXOffset, y - this.tableYOffset) < 50) {
@@ -60,9 +77,9 @@ class EditorScreen extends GUI {
         fill(100)
         rect(410, -OFFSET.y, 290 + OFFSET.x, OFFSET.y * 2 + 400)
         //draw menu
-        let vertOffset = 50
+        let vertOffset = 50 - OFFSET.y
         for (let i = 0; i < this.itemsAvailable.length; i++) {
-            this.itemsAvailable[i].DrawSymbol(405 + OFFSET.x, vertOffset)
+            this.itemsAvailable[i].DrawSymbol(415, vertOffset)
             vertOffset += this.itemsAvailable[i].getHeight() * 2;
         }
         //Draw selected item
@@ -73,11 +90,21 @@ class EditorScreen extends GUI {
                 this.itemSelected.x = x - this.tableXOffset;
                 this.itemSelected.y = y - this.tableYOffset;
             } else if (this.itemTypeSelected == "table") {
-
                 this.tableXOffset += x - this.itemSelected.x
                 this.tableYOffset += y - this.itemSelected.y
                 this.itemSelected.x = x;
                 this.itemSelected.y = y;
+            } else if (this.itemTypeSelected == "inputNode") {
+                push()
+                stroke(0)
+                strokeWeight(5)
+                line(
+                    this.itemSelected.obj.GetInputPosition(this.itemSelected.idx).x + this.tableXOffset,
+                    this.itemSelected.obj.GetInputPosition(this.itemSelected.idx).y + this.tableYOffset,
+                    x,
+                    min(y, this.itemSelected.obj.GetInputPosition(this.itemSelected.idx).y + this.tableXOffset)
+                )
+                pop()
             }
         }
         fill(0)
@@ -100,6 +127,11 @@ class EditorScreen extends GUI {
             for (let i = 0; i < this.tableObjects.length; i++) {
                 for (let j = 0; j < this.tableObjects[i].inputs.length; j++) {
                     let pos = this.tableObjects[i].GetInputPosition(j);
+                    if (dist(pos.x, pos.y, x - this.tableXOffset, y - this.tableYOffset) < 15) {
+                        this.itemTypeSelected = "inputNode"
+                        this.itemSelected = { obj: this.tableObjects[i], idx: j }
+                        return;
+                    }
                 }
                 for (let j = 0; j < this.tableObjects[i].outputs.length; j++) {
                     let pos = this.tableObjects[i].GetOutputPosition(j);
@@ -113,9 +145,9 @@ class EditorScreen extends GUI {
             }
         } else if (x > 410) {
             //select "menu" object
-            let vertOffset = 50
+            let vertOffset = 50 - OFFSET.y
             for (let i = 0; i < this.itemsAvailable.length; i++) {
-                if (mouseInRange(450, vertOffset, this.itemsAvailable[i].getWidth(), this.itemsAvailable[i].getHeight())) {
+                if (mouseInRange(415, vertOffset, this.itemsAvailable[i].getWidth(), this.itemsAvailable[i].getHeight())) {
                     this.itemSelected = this.itemsAvailable[i];
                     this.itemTypeSelected = "blueprint";
                     return;
